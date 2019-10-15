4fc94c29c6609010521a850a5c9d5cf804cd95ef
omardiazdiaz91 <omardiazdiaz91@gmail.com>
2019-10-15 13:10:58 -0700

    Cliente Servidor aplicacion

---
+++ src/TestCliente.java
@@ -0,0 +1,6 @@
+public class TestCliente {
+    public static void main(String[] args) {
+        Cliente cliente =new Cliente();
+        cliente.mensaje_Saliente("10.81.62.219", "readFile",9001);
+    }
+}
